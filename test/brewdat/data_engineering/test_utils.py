f265a769b5c2f63c6f78e00b730743b78713dcd9
Wesley Felipe <wesley.silva@gavb.com.br>
2022-05-27 13:33:50 -0300

    Unit tests with pytest

--- test/brewdat/data_engineering/test_utils.py
+++ test/brewdat/data_engineering/test_utils.py
@@ -6,6 +6,7 @@ from brewdat.data_engineering.utils import BrewDatLibrary
 
 brewdat_library = BrewDatLibrary(spark=spark, dbutils=None)
 
+
 def test_clean_column_names():
     # ARRANGE
     df = spark.createDataFrame([
@@ -16,13 +17,14 @@ def test_clean_column_names():
          }
     ])
 
-    #ACT
+    # ACT
     result_df = brewdat_library.clean_column_names(df)
 
-    #ASSERT
+    # ASSERT
     assert 'phone_number' in result_df.columns
     assert 'name_Complete_' in result_df.columns
     assert 'address_1' in result_df.columns
+
 
 def test_clean_column_names_except_for():
     # ARRANGE
@@ -34,27 +36,59 @@ def test_clean_column_names_except_for():
          }
     ])
 
-    #ACT
+    # ACT
     result_df = brewdat_library.clean_column_names(df, except_for=['address 1'])
 
-    #ASSERT
+    # ASSERT
     assert 'phone_number' in result_df.columns
     assert 'name_Complete_' in result_df.columns
     assert 'address 1' in result_df.columns
 
+
 def test_read_raw_dataframe_csv():
-    #ARRANGE
+    # ARRANGE
     file_location = "./test/brewdat/data_engineering/support_files/my_data_extraction.csv"
 
-    #ACT
+    # ACT
     df = brewdat_library.read_raw_dataframe(
         file_format=BrewDatLibrary.RawFileFormat.CSV
         , location=file_location)
 
-    #ASSERT
+    # ASSERT
     df.show()
 
     assert 2 == df.count()
     assert 'name' in df.columns
     assert 'address' in df.columns
     assert 'phone' in df.columns
+
+
+def test_write_delta_table_append_all(tmpdir):
+
+    # ARRANGE
+    df = spark.createDataFrame([
+        {
+            "phone_number": '00000000000'
+            , "name": 'my name'
+            , "address": "my address"
+        }
+    ])
+    location = f'{tmpdir}/test_write_delta_table_append_all'
+    schema_name = "test_schema"
+    table_name = "test_write_delta_table_append_all"
+
+    # ACT
+    result = brewdat_library.write_delta_table(
+        df=df
+        , location=location
+        , schema_name=schema_name
+        , table_name=table_name
+        , load_type=brewdat_library.LoadType.APPEND_ALL
+    )
+
+    # ASSERT
+    print(result)
+    assert result['status'] == brewdat_library.RunStatus.SUCCEEDED
+
+    result_df = spark.table(result['target_object'])
+    assert 1 == result_df.count()
