d3d281b17ec72526a03978dbdb09435b0cae5b34
Wesley Felipe <wesley.silva@gavb.com.br>
2022-06-22 09:34:21 -0300

    Unit tests for create_or_replace_audit_columns

--- test/brewdat/data_engineering/test_transform_utils.py
+++ test/brewdat/data_engineering/test_transform_utils.py
@@ -1,6 +1,8 @@
 from test.spark_test import spark
 
-from brewdat.data_engineering.transform_utils import clean_column_names
+from datetime import datetime
+
+from brewdat.data_engineering.transform_utils import clean_column_names, create_or_replace_audit_columns
 
 
 def test_clean_column_names():
@@ -39,3 +41,48 @@ def test_clean_column_names_except_for():
     assert "phone_number" in result_df.columns
     assert "name_Complete_" in result_df.columns
     assert "address 1" in result_df.columns
+
+
+def test_create_or_replace_audit_columns():
+    # ARRANGE
+    df = spark.createDataFrame([
+        {
+            "phone  number": "00000000000",
+            "name (Complete)": "my name",
+            "address 1": "my address",
+        }
+    ])
+    # ACT
+    result_df = create_or_replace_audit_columns(dbutils=None, df=df, )
+    # ASSERT
+    assert "__insert_gmt_ts" in result_df.columns
+    assert "__update_gmt_ts" in result_df.columns
+    assert result_df.filter("__insert_gmt_ts is null").count() == 0
+    assert result_df.filter("__update_gmt_ts is null").count() == 0
+
+
+def test_create_or_replace_audit_columns_already_exist():
+    # ARRANGE
+    df = spark.createDataFrame([
+        {
+            "phone  number": "00000000000",
+            "name (Complete)": "my name",
+            "address 1": "my address",
+            "__insert_gmt_ts": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+
+        },
+        {
+            "phone  number": "00000000000",
+            "name (Complete)": "my name",
+            "address 1": "my address",
+            "__insert_gmt_ts": None,
+
+        }
+    ])
+    # ACT
+    result_df = create_or_replace_audit_columns(dbutils=None, df=df, )
+    # ASSERT
+    assert "__insert_gmt_ts" in result_df.columns
+    assert "__update_gmt_ts" in result_df.columns
+    assert result_df.filter("__insert_gmt_ts is null").count() == 0
+    assert result_df.filter("__update_gmt_ts is null").count() == 0
