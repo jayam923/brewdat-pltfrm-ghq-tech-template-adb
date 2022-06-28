6edabb11c442cda68c6c8245f3745c5596d64cba
anvesh.siddareddy@ab-inbev.com <anvesh.siddareddy@ab-inbev.com>
2022-06-28 10:21:11 +0000

    test_business_audit_coloumns
    test_business_audit_coloumns

--- test/brewdat/data_engineering/test_transform_utils.py
+++ test/brewdat/data_engineering/test_transform_utils.py
@@ -86,3 +86,22 @@ def test_create_or_replace_audit_columns_already_exist():
     assert "__update_gmt_ts" in result_df.columns
     assert result_df.filter("__insert_gmt_ts is null").count() == 0
     assert result_df.filter("__update_gmt_ts is null").count() == 0
+    
+def test_create_or_replace_business_key_column():
+    # ARRANGE
+    df=spark.createdataframe([
+         {
+            "phone  number": "00000000000",
+            "name (Complete)": "my name",
+            "address 1": "my address",
+         }
+    ])
+    
+    #ACT
+    result_df = create_or_replace_business_key_column(dbutils=None, df=df, business_key_column_name='exact location', key_columns=["Hyd","Ameerpet"], separator: "__", check_null_values= True )
+    
+    # ASSERT
+    assert "phone_number" in result_df.columns
+    assert "name_Complete_" in result_df.columns
+    assert "address 1" in result_df.columns
+    assert "business_key_column_name"  in result_df.columns
