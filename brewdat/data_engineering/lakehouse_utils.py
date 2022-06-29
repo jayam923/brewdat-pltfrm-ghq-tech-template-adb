1abce30ba6d7227017771fe2c67dbd177f609f16
shivam.yadav-ext@ab-inbev.com <shivam.yadav-ext@ab-inbev.com>
2022-06-29 08:02:27 +0000

    Created different function for Validation
    I have added 3 functions and called those functions in *table_location functions

--- brewdat/data_engineering/lakehouse_utils.py
+++ brewdat/data_engineering/lakehouse_utils.py
@@ -39,6 +39,11 @@ def generate_bronze_table_location(
         params_list = [lakehouse_bronze_root, target_zone, target_business_domain, source_system, table_name]
         if any(x is None or len(x) == 0 for x in params_list):
             raise ValueError("Location would contain null or empty values.")
+        
+        check_zone(target_zone)
+        check_domain(target_business_domain)
+        check_table(table_name)
+        
 
         return f"{lakehouse_bronze_root}/data/{target_zone}/{target_business_domain}/{source_system}/{table_name}".lower()
 
@@ -83,6 +88,10 @@ def generate_silver_table_location(
         params_list = [lakehouse_silver_root, target_zone, target_business_domain, source_system, table_name]
         if any(x is None or len(x) == 0 for x in params_list):
             raise ValueError("Location would contain null or empty values.")
+        
+        check_zone(target_zone)
+        check_domain(target_business_domain)
+        check_table(table_name)
 
         return f"{lakehouse_silver_root}/data/{target_zone}/{target_business_domain}/{source_system}/{table_name}".lower()
 
@@ -130,6 +139,10 @@ def generate_gold_table_location(
         params_list = [lakehouse_gold_root, target_zone, target_business_domain, project, database_name, table_name]
         if any(x is None or len(x) == 0 for x in params_list):
             raise ValueError("Location would contain null or empty values.")
+            
+        check_zone(target_zone)
+        check_domain(target_business_domain)
+        check_table(table_name)
 
         return f"{lakehouse_gold_root}/data/{target_zone}/{target_business_domain}/{project}/{database_name}/{table_name}".lower()
 
@@ -139,150 +152,23 @@ def generate_gold_table_location(
 
         
     
-def check_param_generate_bronze_table_location(
-    dbutils: object,
-    lakehouse_bronze_root: str,
-    target_zone: str,
-    target_business_domain: str,
-    source_system: str,
-    table_name: str,
-) -> str:
-    
-    """Build the standard location for a bronze table.
-
-    Parameters
-    ----------
-    dbutils : object
-        A Databricks utils object.
-    lakehouse_bronze_root : str
-        Root path to the Lakehouse's bronze layer.
-        Format: "abfss://bronze@storage_account.dfs.core.windows.net".
-        Value varies by environment, so you should use environment variables.
-    target_zone : str
-        Zone of the target dataset.
-    target_business_domain : str
-        Business domain of the target dataset.
-    source_system : str
-        Name of the source system.
-    table_name : str
-        Name of the target table in the metastore.
-
-    Returns
-    -------
-    str
-        Standard location for the delta table.
-    """
-   
-        # Check that no parameter is None or empty string
-    params_list = [lakehouse_bronze_root, target_zone, target_business_domain, source_system, table_name]
-    expected=["afr", "apac", "eur", "ghq", "maz", "naz", "saz"]
-    domain=["compliance","finance","marketing","people","sales","supply","tech"]
-    #
-    if target_zone not in expected:
+def check_zone(target_zone):
+    expected_zone=["afr", "apac", "eur", "ghq", "maz", "naz", "saz"]
+    if target_zone not in expected_zone:
         raise ValueError("Zone does not have expected value.")
-    if target_business_domain not in domain:
+
+
+
+
+
+def check_domain(target_business_domain):
+    expected_domain=["compliance","finance","marketing","people","sales","supply","tech"]
+    if target_business_domain not in expected_domain:
         raise ValueError("Domain does not have expected value.")
+        
+
+
+def check_table(table_name):
     if bool(re.match('[a-zA-Z0-9][a-zA-Z0-9\.\_\-]+$', table_name)) !=True:
         raise ValueError("Folder should start with alphanumeric characters.")
         
-    return f"{lakehouse_bronze_root}/data/{target_zone}/{target_business_domain}/{source_system}/{table_name}".lower()
-
-
-
-def check_param_generate_silver_table_location(
-    dbutils: object,
-    lakehouse_silver_root: str,
-    target_zone: str,
-    target_business_domain: str,
-    source_system: str,
-    table_name: str,
-) -> str:
-    
-    """Build the standard location for a Silver table.
-
-    Parameters
-    ----------
-    dbutils : object
-        A Databricks utils object.
-    lakehouse_silver_root : str
-        Root path to the Lakehouse's Silver layer.
-        Format: "abfss://silver@storage_account.dfs.core.windows.net".
-        Value varies by environment, so you should use environment variables.
-    target_zone : str
-        Zone of the target dataset.
-    target_business_domain : str
-        Business domain of the target dataset.
-    source_system : str
-        Name of the source system.
-    table_name : str
-        Name of the target table in the metastore.
-
-    Returns
-    -------
-    str
-        Standard location for the delta table.
-    """
-   
-        # Check that no parameter is None or empty string
-    params_list = [lakehouse_silver_root, target_zone, target_business_domain, source_system, table_name]
-    expected=["afr", "apac", "eur", "ghq", "maz", "naz", "saz"]
-    domain=["compliance","finance","marketing","people","sales","supply","tech"]
-    #
-    if target_zone not in expected:
-        raise ValueError("Zone does not have expected value.")
-    if target_business_domain not in domain:
-        raise ValueError("Domain does not have expected value.")
-    if bool(re.match('[a-zA-Z0-9][a-zA-Z0-9\.\_\-]+$', table_name)) !=True:
-        raise ValueError("Folder should start with alphanumeric characters.")
-        
-    return f"{lakehouse_silver_root}/data/{target_zone}/{target_business_domain}/{source_system}/{table_name}".lower()
-
-
-
-def check_param_generate_gold_table_location(
-    dbutils: object,
-    lakehouse_gold_root: str,
-    target_zone: str,
-    target_business_domain: str,
-    source_system: str,
-    table_name: str,
-) -> str:
-    
-    """Build the standard location for a gold table.
-
-    Parameters
-    ----------
-    dbutils : object
-        A Databricks utils object.
-    lakehouse_gold_root : str
-        Root path to the Lakehouse's gold layer.
-        Format: "abfss://gold@storage_account.dfs.core.windows.net".
-        Value varies by environment, so you should use environment variables.
-    target_zone : str
-        Zone of the target dataset.
-    target_business_domain : str
-        Business domain of the target dataset.
-    source_system : str
-        Name of the source system.
-    table_name : str
-        Name of the target table in the metastore.
-
-    Returns
-    -------
-    str
-        Standard location for the delta table.
-    """
-   
-        # Check that no parameter is None or empty string
-    params_list = [lakehouse_gold_root, target_zone, target_business_domain, source_system, table_name]
-    expected=["afr", "apac", "eur", "ghq", "maz", "naz", "saz"]
-    domain=["compliance","finance","marketing","people","sales","supply","tech"]
-    #
-    if target_zone not in expected:
-        raise ValueError("Zone does not have expected value.")
-    if target_business_domain not in domain:
-        raise ValueError("Domain does not have expected value.")
-    if bool(re.match('[a-zA-Z0-9][a-zA-Z0-9\.\_\-]+$', table_name)) !=True:
-        raise ValueError("Folder should start with alphanumeric characters.")
-        
-    return f"{lakehouse_gold_root}/data/{target_zone}/{target_business_domain}/{source_system}/{table_name}".lower()
