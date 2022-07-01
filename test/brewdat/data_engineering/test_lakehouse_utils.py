fe13af6c6bd25b283a7040f26d1bdd09a5147427
SabadiniAbreu <joao.abreu@gavb.com.br>
2022-07-01 14:29:33 -0300

    Creating lakehouse_utils_test

--- test/brewdat/data_engineering/test_lakehouse_utils.py
+++ test/brewdat/data_engineering/test_lakehouse_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from brewdat.data_engineering.lakehouse_utils import check_table_name
+from brewdat.data_engineering.lakehouse_utils import check_table_name, generate_bronze_table_location
 
 
 def test_check_table_name_valid_names():
@@ -24,3 +24,14 @@ def test_check_table_name_invalid_name2():
 def test_check_table_name_invalid_name3():
     with pytest.raises(ValueError):
         check_table_name("new_table_ç")
+ 
+
+
+def test_generate_bronze_table_location():
+    # ARRANGE
+    
+    # ACT
+    result = generate_bronze_table_location(dbutils=None,lakehouse_bronze_root= "data"/{"target_zone"}/{"target_business_domain"}/{"source_system"}/{"table_name"})
+    
+    # ASSERT
+    assert "lakehouse_bronze_root" in result.columns
