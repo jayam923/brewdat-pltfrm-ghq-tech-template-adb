0b21ceb4c3154c9e4981779cf16d8003b50ca77c
Wesley Felipe <wesley.silva@gavb.com.br>
2022-07-12 08:32:00 -0300

    Fix unit test for test_write_duplicated_data_for_upsert

--- test/brewdat/data_engineering/test_write_utils.py
+++ test/brewdat/data_engineering/test_write_utils.py
@@ -713,13 +713,12 @@ def test_write_duplicated_data_for_upsert(tmpdir):
     schema_name = "test_schema"
     table_name = "test_write_duplicated_data_for_upsert"
 
-    expected_message = "java.lang.UnsupportedOperationException: Cannot perform Merge as multiple source rows " \
+    expected_message_start = "java.lang.UnsupportedOperationException: Cannot perform Merge as multiple source rows " \
                        "matched and attempted to modify the same target row in the Delta table in possibly " \
                        "conflicting ways. By SQL semantics of Merge, when multiple source rows match on the same " \
                        "target row, the result may be ambiguous as it is unclear which source row should be " \
                        "used to update or delete the matching target row. You can preprocess the source table to " \
                        "eliminate the possibility of multiple matches. Please refer to " \
-                       "https://docs.delta.io/latest/delta-update.html#upsert-into-a-table-using-merge"
 
     # ACT
     result = write_delta_table(
@@ -746,4 +745,4 @@ def test_write_duplicated_data_for_upsert(tmpdir):
     print(vars(result))
 
     assert result.status == RunStatus.FAILED
-    assert result.error_message == expected_message
+    assert result.error_message.startswith(expected_message_start)
