49b808e78f884dd22c261dc50ffcc202a7b49a91
SabadiniAbreu <joao.abreu@gavb.com.br>
2022-07-18 15:06:05 -0300

    Test without watermark

--- test/brewdat/data_engineering/test_transform_utils.py
+++ test/brewdat/data_engineering/test_transform_utils.py
@@ -907,7 +907,7 @@ def test_deduplicate_records_different_keys():
             "date": "2022-11-22"
             
         },
-         
+        
         {
             "name": "joao",
             "last name": "abreu2",
@@ -922,6 +922,35 @@ def test_deduplicate_records_different_keys():
     ])
     # ACT
     result_df = deduplicate_records(dbutils=None, df=df,key_columns=["name","last name"], watermark_column = "date")
+    #result_df.show()
+    # ASSERT
+    assert 2 == result_df.count()
+
+
+def test_deduplicate_records_without_watermark_column():
+    # ARRANGE
+    df = spark.createDataFrame([
+        {
+            "name": "joao",
+            "last name": "abreu",
+            "date": "2022-11-22"
+            
+        },
+        
+        {
+            "name": "joao",
+            "last name": "abreu2",
+            "date": "2022-11-22",
+            
+        },
+        {
+            "name": "joao",
+            "last name": "abreu",
+            "date": "2022-11-22",
+        },
+    ])
+    # ACT
+    result_df = deduplicate_records(dbutils=None, df=df,key_columns=["name","last name"])
     #result_df.show()
     # ASSERT
     assert 2 == result_df.count()
