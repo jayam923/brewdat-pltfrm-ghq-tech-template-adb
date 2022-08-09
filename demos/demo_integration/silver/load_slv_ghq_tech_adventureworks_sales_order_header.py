3ef8863f7221d6c251e7079782719b02bb4aa7fc
DanielBrum-ABI <DanielBrum-ABI>
2022-08-09 17:47:54 +0000

    Updated demo silver notebook

--- demos/demo_integration/silver/load_slv_ghq_tech_adventureworks_sales_order_header.py
+++ demos/demo_integration/silver/load_slv_ghq_tech_adventureworks_sales_order_header.py
@@ -33,12 +33,11 @@ print(f"data_interval_end: {data_interval_end}")
 
 # COMMAND ----------
 
-import os
 import sys
 
 # Import BrewDat Library modules
 sys.path.append(f"/Workspace/Repos/brewdat_library/{brewdat_library_version}")
-from brewdat.data_engineering import common_utils, lakehouse_utils, transform_utils, write_utils
+from brewdat.data_engineering import common_utils, data_quality_utils, lakehouse_utils, transform_utils, write_utils
 
 # Print a module's help
 help(transform_utils)
@@ -60,57 +59,99 @@ common_utils.configure_spn_access_for_adls(
 
 # COMMAND ----------
 
-key_columns = ["sales_order_id"]
+from pyspark.sql import functions as F
 
-df = spark.sql("""
-        SELECT
-            CAST(SalesOrderId AS INT) AS sales_order_id,
-            CAST(RevisionNumber AS TINYINT) AS revision_number,
-            TO_DATE(OrderDate) AS order_date,
-            TO_DATE(DueDate) AS due_date,
-            TO_DATE(ShipDate) AS ship_date,
-            CAST(Status AS TINYINT) AS status_code,
-            CASE
-                WHEN Status = 1 THEN 'In Process'
-                WHEN Status = 2 THEN 'Approved'
-                WHEN Status = 3 THEN 'Backordered'
-                WHEN Status = 4 THEN 'Rejected'
-                WHEN Status = 5 THEN 'Shipped'
-                WHEN Status = 6 THEN 'Canceled'
-                WHEN Status IS NULL THEN NULL
-                ELSE '--MAPPING ERROR--'
-            END AS status,
-            CAST(OnlineOrderFlag AS BOOLEAN) AS online_order_flag,
-            SalesOrderNumber AS sales_order_number,
-            PurchaseOrderNumber AS purchase_order_number,
-            AccountNumber AS account_number,
-            CAST(CustomerId AS INT) AS customer_id,
-            CAST(ShipToAddressId AS INT) AS ship_to_address_id,
-            CAST(BillToAddressId AS INT) AS bill_to_address_id,
-            ShipMethod AS ship_method,
-            CAST(SubTotal AS DECIMAL(19,4)) AS sub_total,
-            CAST(TaxAmt AS DECIMAL(19,4)) AS tax_amount,
-            CAST(Freight AS DECIMAL(19,4)) AS freight,
-            CAST(TotalDue AS DECIMAL(19,4)) AS total_due,
-            TO_TIMESTAMP(ModifiedDate) AS modified_date,
-            __ref_dt
-        FROM
-            brz_ghq_tech_adventureworks.sales_order_header
-        WHERE 1 = 1
-            AND __ref_dt BETWEEN DATE_FORMAT('{data_interval_start}', 'yyyyMMdd')
-                AND DATE_FORMAT('{data_interval_end}', 'yyyyMMdd')
-    """.format(
-        data_interval_start=data_interval_start,
-        data_interval_end=data_interval_end,
+key_columns = ["SalesOrderID"]
+
+bronze_df = (
+    spark.read
+    .table("brz_ghq_tech_adventureworks.sales_order_header")
+    .filter(F.col("__ref_dt").between(
+        F.date_format(F.lit(data_interval_start), "yyyyMMdd"),
+        F.date_format(F.lit(data_interval_end), "yyyyMMdd")
     ))
+)
 
-#display(df)
+#display(bronze_df)
+
+# COMMAND ----------
+
+dq_df = bronze_df
+data_quality_utils.check_columns_exist(dbutils=dbutils, df=dq_df, column_names=["SalesOrderID", "CustomerID"])
+dq_df = data_quality_utils.check_column_is_not_null(dbutils=dbutils, df=dq_df, column_name="SalesOrderID")
+dq_df = data_quality_utils.check_column_is_not_null(dbutils=dbutils, df=dq_df, column_name="CustomerID")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="SalesOrderID", data_type="int")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="RevisionNumber", data_type="tinyint")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="OrderDate", data_type="date")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="DueDate", data_type="date")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="ShipDate", data_type="date")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="Status", data_type="tinyint")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="OnlineOrderFlag", data_type="boolean")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="CustomerID", data_type="int")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="ShipToAddressID", data_type="int")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="BillToAddressID", data_type="int")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="SubTotal", data_type="decimal(19,4)")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="TaxAmt", data_type="decimal(19,4)")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="Freight", data_type="decimal(19,4)")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="TotalDue", data_type="decimal(19,4)")
+dq_df = data_quality_utils.check_column_type_cast(dbutils=dbutils, df=dq_df, column_name="ModifiedDate", data_type="timestamp")
+dq_df = data_quality_utils.check_column_value_is_in(dbutils=dbutils, df=dq_df, column_name="Status", valid_values=[1, 2, 3, 4, 5, 6])
+dq_df = data_quality_utils.check_column_max_length(dbutils=dbutils, df=dq_df, column_name="SalesOrderNumber", maximum_length=30)
+dq_df = data_quality_utils.check_column_max_length(dbutils=dbutils, df=dq_df, column_name="PurchaseOrderNumber", maximum_length=30)
+dq_df = data_quality_utils.check_column_max_length(dbutils=dbutils, df=dq_df, column_name="ShipMethod", maximum_length=100)
+dq_df = data_quality_utils.check_column_max_length(dbutils=dbutils, df=dq_df, column_name="AccountNumber", maximum_length=15)
+dq_df = data_quality_utils.check_column_matches_regular_expression(dbutils=dbutils, df=dq_df, column_name="AccountNumber", regular_expression="^\d{2}-\d{4}-\d{6}$")
+
+dq_df.createOrReplaceTempView("v_bronze_dq_df")
+
+#display(dq_df)
+
+# COMMAND ----------
+
+transformed_df = spark.sql("""
+    SELECT
+        CAST(SalesOrderID AS INT) AS SalesOrderID,
+        CAST(RevisionNumber AS TINYINT) AS RevisionNumber,
+        TO_DATE(OrderDate) AS OrderDate,
+        TO_DATE(DueDate) AS DueDate,
+        TO_DATE(ShipDate) AS ShipDate,
+        CAST(Status AS TINYINT) AS Status,
+        CASE
+            WHEN Status = 1 THEN 'In Process'
+            WHEN Status = 2 THEN 'Approved'
+            WHEN Status = 3 THEN 'Backordered'
+            WHEN Status = 4 THEN 'Rejected'
+            WHEN Status = 5 THEN 'Shipped'
+            WHEN Status = 6 THEN 'Canceled'
+            WHEN Status IS NULL THEN NULL
+            ELSE '--MAPPING ERROR--'
+        END AS StatusDescription,
+        CAST(OnlineOrderFlag AS BOOLEAN) AS OnlineOrderFlag,
+        SalesOrderNumber AS SalesOrderNumber,
+        PurchaseOrderNumber AS PurchaseOrderNumber,
+        AccountNumber AS AccountNumber,
+        CAST(CustomerID AS INT) AS CustomerID,
+        CAST(ShipToAddressID AS INT) AS ShipToAddressID,
+        CAST(BillToAddressID AS INT) AS BillToAddressID,
+        ShipMethod,
+        CAST(SubTotal AS DECIMAL(19,4)) AS SubTotal,
+        CAST(TaxAmt AS DECIMAL(19,4)) AS TaxAmt,
+        CAST(Freight AS DECIMAL(19,4)) AS Freight,
+        CAST(TotalDue AS DECIMAL(19,4)) AS TotalDue,
+        TO_TIMESTAMP(ModifiedDate) AS ModifiedDate,
+        __ref_dt,
+        __data_quality_issues
+    FROM
+        v_bronze_dq_df
+""")
+
+#display(transformed_df)
 
 # COMMAND ----------
 
 dedup_df = transform_utils.deduplicate_records(
     dbutils=dbutils,
-    df=df,
+    df=transformed_df,
     key_columns=key_columns,
     watermark_column="__ref_dt",
 )
