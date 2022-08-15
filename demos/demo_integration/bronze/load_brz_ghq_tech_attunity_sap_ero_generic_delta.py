71cc9b737ef8b9bebb8be5b460c51daf2770e440
DanielBrum-ABI <DanielBrum-ABI>
2022-08-15 17:14:59 +0000

    Clean-up bronze notebook

--- demos/demo_integration/bronze/load_brz_ghq_tech_attunity_sap_ero_generic_delta.py
+++ demos/demo_integration/bronze/load_brz_ghq_tech_attunity_sap_ero_generic_delta.py
@@ -35,7 +35,7 @@ print(f"prelz_path: {prelz_path}")
 
 import sys
 from pyspark.sql import functions as F
-from datetime import datetime
+
 # Import BrewDat Library modules
 sys.path.append(f"/Workspace/Repos/brewdat_library/{brewdat_library_version}")
 from brewdat.data_engineering import common_utils, lakehouse_utils, read_utils, transform_utils, write_utils
@@ -55,7 +55,6 @@ common_utils.configure_spn_access_for_adls(
     dbutils=dbutils,
     storage_account_names=[
         adls_raw_bronze_storage_account_name,
-        adls_silver_gold_storage_account_name,
         adls_brewdat_ghq_storage_account_name,
     ],
     key_vault_name=key_vault_name,
@@ -65,56 +64,45 @@ common_utils.configure_spn_access_for_adls(
 
 # COMMAND ----------
 
-base_raw_df = read_utils.read_raw_dataframe(
-    spark=spark,
-    dbutils=dbutils,
-    file_format=read_utils.RawFileFormat.DELTA,
-    location=f"{attunity_sap_root}/{prelz_path}",
-    cast_all_to_string=False,
+base_df = (
+    spark.read
+    .format("delta")
+    .load(f"{brewdat_ghq_root}/{prelz_path}")
 )
-max_base_watermark_value = base_raw_df.select(F.max(F.col("TARGET_APPLY_TS"))).collect()[0][0]
-print(max_base_watermark_value)
-#display(base_raw_df)
+
+#display(base_df)
 
 # COMMAND ----------
 
-ct_raw_df = read_utils.read_raw_dataframe(
-    spark=spark,
-    dbutils=dbutils,
-    file_format=read_utils.RawFileFormat.DELTA,
-    location=f"{attunity_sap_root}/{prelz_path}__ct",
-    cast_all_to_string=False,
+ct_df = (
+    spark.read
+    .format("delta")
+    .load(f"{brewdat_ghq_root}/{prelz_path}__ct")
+    .filter(F.col("TARGET_APPLY_DT") >= F.to_date(F.lit(data_interval_start)))
 )
-max_ct_watermark_value = ct_raw_df.select(F.max(F.col("TARGET_APPLY_TS"))).collect()[0][0]
-print(max_ct_watermark_value)
-#display(ct_raw_df)
+
+#display(ct_df)
 
 # COMMAND ----------
 
-clean_base_df = transform_utils.clean_column_names(dbutils=dbutils, df=base_raw_df)
-clean_ct_df = transform_utils.clean_column_names(dbutils=dbutils, df=ct_raw_df)
+max_base_watermark_value = base_df.select(F.max(F.col("TARGET_APPLY_TS")).cast("string")).collect()[0][0]
+print(f"max_base_watermark_value: {max_base_watermark_value}")
 
-# COMMAND ----------
+max_ct_watermark_value = ct_df.select(F.max(F.col("TARGET_APPLY_TS")).cast("string")).collect()[0][0]
+print(f"max_ct_watermark_value: {max_ct_watermark_value}")
 
-default_data_interval_end = datetime.strptime(data_interval_start, "%Y-%m-%d %H:%M:%S.%f")
-base_watermark_upper_bound = max_base_watermark_value if max_base_watermark_value else default_data_interval_end
-ct_watermark_upper_bound = max_ct_watermark_value if max_ct_watermark_value else default_data_interval_end
-watermark_upper_bound = ct_watermark_upper_bound if ct_watermark_upper_bound > base_watermark_upper_bound else base_watermark_upper_bound
-data_interval_end = watermark_upper_bound.strftime("%Y-%m-%d %H:%M:%S.%f")
-print(data_interval_end)
+effective_data_interval_end = max_ct_watermark_value or max_base_watermark_value or data_interval_start
+print(f"effective_data_interval_end: {effective_data_interval_end}")
 
 # COMMAND ----------
 
 filtered_base_df = (
-    clean_base_df
-    .filter(F.col("TARGET_APPLY_DT").between(
-        F.to_timestamp(F.lit(data_interval_start)),
-        F.to_timestamp(F.lit(max_base_watermark_value)),
-    ))
+    base_df
     .filter(F.col("TARGET_APPLY_TS").between(
         F.to_timestamp(F.lit(data_interval_start)),
-        F.to_timestamp(F.lit(max_base_watermark_value)),
+        F.to_timestamp(F.lit(effective_data_interval_end)),
     ))
+    .withColumn("__src_file", F.input_file_name())
 )
 
 #display(filtered_base_df)
@@ -122,37 +110,43 @@ filtered_base_df = (
 # COMMAND ----------
 
 filtered_ct_df = (
-    clean_ct_df
-    .filter(F.col("TARGET_APPLY_DT").between(
-        F.to_date(F.lit(data_interval_start)),
-        F.to_date(F.lit(max_ct_watermark_value)),
-    ))
+    ct_df
     .filter(F.col("TARGET_APPLY_TS").between(
         F.to_timestamp(F.lit(data_interval_start)),
-        F.to_timestamp(F.lit(max_ct_watermark_value)),
+        F.to_timestamp(F.lit(effective_data_interval_end)),
     ))
     # Ignore "Before Image" records from update operations
     .filter("header__change_oper != 'B'")
+    .withColumn("__src_file", F.input_file_name())
 )
 
 #display(filtered_ct_df)
 
 # COMMAND ----------
 
-union_df = filtered_base_df.unionByName(filtered_ct_df, allowMissingColumns=True)
+clean_base_df = transform_utils.clean_column_names(dbutils=dbutils, df=filtered_base_df)
+clean_ct_df = transform_utils.clean_column_names(dbutils=dbutils, df=filtered_ct_df)
+
+#display(clean_base_df)
+#display(clean_ct_df)
 
 # COMMAND ----------
 
-transformed_df = (
-    transform_utils.cast_all_columns_to_string(dbutils=dbutils, df=union_df)
-    .withColumn("__src_file", F.input_file_name())
-)
+transformed_base_df = transform_utils.cast_all_columns_to_string(dbutils=dbutils, df=clean_base_df)
+transformed_ct_df = transform_utils.cast_all_columns_to_string(dbutils=dbutils, df=clean_ct_df)
 
-#display(transformed_df)
+#display(transformed_base_df)
+#display(transformed_ct_df)
 
 # COMMAND ----------
 
-audit_df = transform_utils.create_or_replace_audit_columns(dbutils=dbutils, df=transformed_df)
+union_df = transformed_base_df.unionByName(transformed_ct_df, allowMissingColumns=True)
+
+#display(union_df)
+
+# COMMAND ----------
+
+audit_df = transform_utils.create_or_replace_audit_columns(dbutils=dbutils, df=union_df)
 
 #display(audit_df)
 
@@ -166,23 +160,18 @@ target_location = lakehouse_utils.generate_bronze_table_location(
     source_system=source_system,
     table_name=target_hive_table,
 )
+print(target_location)
 
 results = write_utils.write_delta_table(
     spark=spark,
     df=audit_df,
     location=target_location,
-    schema_name=target_hive_database,
+    database_name=target_hive_database,
     table_name=target_hive_table,
     load_type=write_utils.LoadType.APPEND_ALL,
     partition_columns=["TARGET_APPLY_DT"],
-    schema_evolution_mode=write_utils.SchemaEvolutionMode.ADD_NEW_COLUMNS
+    schema_evolution_mode=write_utils.SchemaEvolutionMode.ADD_NEW_COLUMNS,
 )
-
-results.data_interval_start = data_interval_start
-results.data_interval_end = data_interval_end
-
-print(vars(results))
-
-# COMMAND ----------
-
+results.effective_data_interval_start = data_interval_start
+results.effective_data_interval_end = effective_data_interval_end
 common_utils.exit_with_object(dbutils=dbutils, results=results)
